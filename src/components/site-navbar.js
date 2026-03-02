b9a61cc91f45491ad22bcb8d0c7a195c1bcfbc58
andn123 <andrewni0123@gmail.com>
2026-03-02 01:35:28 -0800

    added search.html

--- src/components/site-navbar.js
+++ src/components/site-navbar.js
@@ -29,7 +29,7 @@ class SiteNavbar extends HTMLElement {
                                 <a class="nav-link" href="/">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/">Search</a>
+                                <a class="nav-link" href="/pages/search.html">Search</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="/">Arrivals</a>
